190f74ad13fe95f3575d642d96d82ec493e10286
nik <abc@example.com>
2025-06-17 10:08:27 +0400

    first cmmit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, This is app deployed usin gAWS CICD-Project'
+
+@app.route('/health')
+def health():
+    return {'status': 'UP'}, 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
